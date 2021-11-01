09fed5baa3a28ed75083fb121ca42f3ee873dd9a
janco <jancoweyers@gmail.com>
2021-11-01 09:29:53 +0200

    Add project files.

---
+++ W3SHARE-Interface/Models/ImageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace W3SHARE_Interface.Models
+{
+    public class ImageModel
+    {
+        public int ID { get; set; }
+        public string  Geolocation { get; set; }
+        public string Tags { get; set; }
+        public DateTime CaptureDate { get; set; }
+        public string CapturedBy { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ W3SHARE-Interface/Pages/Feed.razor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using W3SHARE_Interface.Models;
+
+namespace W3SHARE_Interface.Pages
+{
+    public partial class Feed
+    {
+        public IEnumerable<ImageModel> ImageList { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadImages();
+            return base.OnInitializedAsync();
+        }
+
+        private void LoadImages()
+        {
+
+            ImageModel I_1 = new ImageModel
+            {
+                ID = 1,
+                Geolocation = " Potchefstroom ",
+                Tags = "#Nature #LoveLife",
+                CaptureDate = new DateTime(1980, 10, 5),
+                CapturedBy = "Janco",
+                ImageURL = @"DemoData/1.png"
+            };
+
+            ImageModel I_2 = new ImageModel
+            {
+                ID = 2,
+                Geolocation = " Klerksdorp ",
+                Tags = "#Nature #LoveLife",
+                CaptureDate = new DateTime(1980, 10, 5),
+                CapturedBy = "Janco",
+                ImageURL = @"DemoData/2.png"
+            };
+
+            ImageModel I_3 = new ImageModel
+            {
+                ID = 3,
+                Geolocation = " Parys ",
+                Tags = "#Nature #LoveLife",
+                CaptureDate = new DateTime(1980, 10, 5),
+                CapturedBy = "Janco",
+                ImageURL = "DemoData/3.png"
+            };
+
+            ImageModel I_4 = new ImageModel
+            {
+                ID = 3,
+                Geolocation = " Parys ",
+                Tags = "#Nature #LoveLife",
+                CaptureDate = new DateTime(1980, 10, 5),
+                CapturedBy = "Janco",
+                ImageURL = "DemoData/3.png"
+            };
+
+
+            ImageList = new List<ImageModel> { I_1 , I_2 ,I_3,I_4 };
+        }
+    }
+}
+
+    
